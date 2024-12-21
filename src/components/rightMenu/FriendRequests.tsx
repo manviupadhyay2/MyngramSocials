760bf893c0c37b9b61c93e96b1e9b35287d476c3
Manvi Upadhyay <130921302+manviupadhyay2@users.noreply.github.com>
2024-12-21 11:18:46 +0530

    Update FriendRequests.tsx

--- src/components/rightMenu/FriendRequests.tsx
+++ src/components/rightMenu/FriendRequests.tsx
@@ -23,7 +23,7 @@ const FriendRequests = async () => {
         <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
             {/* TOP */}
             <div className="flex justify-between items-center font-medium">
-                <span className="text-gray-400">Friend Request</span>
+                <span className="text-gray-400">Friend Requests</span>
                 <Link href="/" className="text-blue-500 text-xs">
                     See all
                 </Link>
